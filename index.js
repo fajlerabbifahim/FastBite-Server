a1ecde386ea8b64ee765de3aa073ef510a5d7cf4
shorifulbd1st <shorifulbd1st@gmail.com>
2025-03-24 08:39:03 +0600

    save users info

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o3yie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8jenr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionalSuccessStatus: 200,
+}
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,16 +25,49 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(cors(corsOptions));
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const usersCollection = client.db('Fast-Bite').collection('users');
+
+    app.post('/users', async (req, res) => {
+      const userInfo = req.body;
+      const filter = { email: userInfo.email }
+      const existingUser = await usersCollection.findOne(filter);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result)
+    })
+
+    app.get('/user', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      // console.log(email)
+      const query = { email }
+      // console.log(query)
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    })
+
+
+
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
