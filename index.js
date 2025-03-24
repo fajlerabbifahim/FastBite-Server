8671411b0c5784570edee558db3bcdfe62621cf6
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-03-24 07:51:17 +0600

    menu added and cart items api added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8jenr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j876r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +30,60 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const menuCollection = client.db('fastBite').collection('menu');
+    const cartCollection = client.db('fastBite').collection('cart');
+
+    //get menu items
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
+    //get cart items
+    app.get('/cartItems', async(req, res)=>{
+      const email = req.query.email;
+      const query = {email};
+
+      const result = await cartCollection.findOne(query);
+      res.send(result);
+    })
+
+    // set cart items in database
+    app.put('/cartItems', async (req, res) => {
+      const cartItems = req.body;
+      const email = req.query.email;
+      const query = { email };
+
+      const prevCart = await cartCollection.findOne(query);
+      let updatedCart = {};
+
+      if (prevCart) {
+        updatedCart.email = email;
+
+        Object.keys(prevCart).forEach((key) => {
+          if (key !== "email") {
+            updatedCart[key] = prevCart[key];
+          }
+        });
+
+        Object.keys(cartItems).forEach((key) => {
+          if (key !== "email") {
+            updatedCart[key] = (updatedCart[key] || 0) + cartItems[key];
+          }
+        });
+      } else {
+        updatedCart = { ...cartItems, email }; 
+      }
+
+      const options = { upsert: true };
+      const updatedDoc = { $set: updatedCart };
+
+      const result = await cartCollection.updateOne(query, updatedDoc, options);
+
+      res.send({ success: true, message: "Cart updated successfully", result });
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
