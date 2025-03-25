e8e379e604faf471623e509db06ba7709d599b09
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-03-25 20:27:30 +0600

    changed database uri for use personal database

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o3yie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o3yie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
+//zehad's database uri (*** change for you database ***)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j876r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', '*'],
   credentials: true,
   optionalSuccessStatus: 200,
 }
@@ -34,7 +38,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    const usersCollection = client.db('Fast-Bite').collection('users');
+    const usersCollection = client.db('fastBite').collection('users');
     const menuCollection = client.db('fastBite').collection('menu');
     const cartCollection = client.db('fastBite').collection('cart');
 
